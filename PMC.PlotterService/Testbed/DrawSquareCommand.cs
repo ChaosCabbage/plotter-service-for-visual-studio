bb72848c276342781a81a9bd08847b2b00d1835b
Paul Courts <puke_4@hotmail.com>
2016-02-21 19:56:57 +0000

    Added the Testbed project It's a menu command to draw a simple square on the plotter, for a quick convenient test.

---
+++ PMC.PlotterService/Testbed/DrawSquareCommand.cs
@@ -0,0 +1,127 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="DrawSquareCommand.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+using PMC.PlotterService;
+using PMC.PlotterService.Drawing;
+using System.Collections.Generic;
+
+namespace Testbed
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DrawSquareCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("f41b794e-3749-42c3-a68b-30e1adafdc37");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package _package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DrawSquareCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private DrawSquareCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            _package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DrawSquareCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return _package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new DrawSquareCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            IPlotter2DService plotter = ServiceProvider.GetService(typeof(SPlotter2DService)) as IPlotter2DService;
+
+            if (plotter == null)
+            {
+                string message = string.Format(CultureInfo.CurrentCulture, "The Plotter2DService was not to be found");
+                string title = "DrawSquareCommand";
+
+                // Show a message box to prove we were here
+                VsShellUtilities.ShowMessageBox(
+                    this.ServiceProvider,
+                    message,
+                    title,
+                    OLEMSGICON.OLEMSGICON_CRITICAL,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+
+            var square = new List<PlotterPosition>(5);
+            square.Add(new PlotterPosition { X =  0, Y =  0 });
+            square.Add(new PlotterPosition { X = 10, Y =  0 });
+            square.Add(new PlotterPosition { X = 10, Y = 10 });
+            square.Add(new PlotterPosition { X =  0, Y = 10 });
+            square.Add(new PlotterPosition { X =  0, Y =  0 });
+
+            plotter.AddPointSeries(square);
+        }
+    }
+}
